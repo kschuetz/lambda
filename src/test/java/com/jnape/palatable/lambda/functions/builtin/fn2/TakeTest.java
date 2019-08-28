222aad6e6cf0bbe96f3ef6d5d755c895e028f762
Kevin Schuetz <schuetzk@gmail.com>
2019-08-28 18:43:35 -0500

    Add some tests for Take

--- src/test/java/com/jnape/palatable/lambda/functions/builtin/fn2/TakeTest.java
+++ src/test/java/com/jnape/palatable/lambda/functions/builtin/fn2/TakeTest.java
@@ -10,7 +10,12 @@ import testsupport.traits.FiniteIteration;
 import testsupport.traits.ImmutableIteration;
 import testsupport.traits.Laziness;
 
+import java.util.List;
+
+import static com.jnape.palatable.lambda.functions.builtin.fn2.Drop.drop;
+import static com.jnape.palatable.lambda.functions.builtin.fn2.Iterate.iterate;
 import static com.jnape.palatable.lambda.functions.builtin.fn2.Take.take;
+import static com.jnape.palatable.lambda.monoid.builtin.Concat.concat;
 import static java.util.Arrays.asList;
 import static java.util.Collections.emptyList;
 import static org.junit.Assert.assertThat;
@@ -40,4 +45,64 @@ public class TakeTest {
     public void takesNothingFromEmptyIterable() {
         assertThat(take(1, emptyList()), iterates());
     }
+
+    // new
+
+    @Test
+    public void stackSafety() {
+        int stackBlowingNumber = 10_000;
+        Iterable<Integer> subject = iterate(acc -> acc + 1, 0);
+        for (int i = 1; i <= stackBlowingNumber; i++) {
+            subject = take(i, subject);
+        }
+        assertThat(subject, iterates(0));
+    }
+
+    @Test
+    public void whenCombinedTakesMinimum() {
+        List<Integer> source = asList(1, 2, 3, 4, 5, 6);
+        assertThat(take(5, take(4, take(3, take(2, take(1, source))))),
+                iterates(1));
+        assertThat(take(1, take(2, take(3, take(4, take(5, source))))),
+                iterates(1));
+        assertThat(take(1, take(2, take(0, take(3, take(4, source))))),
+                iterates());
+    }
+
+    @Test
+    public void compoundDrop() {
+        Iterable<Integer> source = drop(0, drop(1, drop(2, drop(3, asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)))));
+        assertThat(take(0, source), iterates());
+        assertThat(take(1, source), iterates(7));
+        assertThat(take(3, source), iterates(7, 8, 9));
+        assertThat(take(100, source), iterates(7, 8, 9, 10));
+    }
+
+    @Test
+    public void compoundConcat() {
+        Iterable<Integer> source = concat(asList(1, 2), concat(asList(3, 4), asList(5, 6)));
+        assertThat(take(0, source), iterates());
+        assertThat(take(1, source), iterates(1));
+        assertThat(take(5, source), iterates(1, 2, 3, 4, 5));
+        assertThat(take(100, source), iterates(1, 2, 3, 4, 5, 6));
+    }
+
+    @Test
+    public void compoundConcatDrop() {
+        Iterable<Integer> source = concat(drop(0, asList(1, 2, 3, 4)),
+                drop(1, concat(drop(2, asList(3, 4, 5, 6)),
+                        drop(3, asList(7, 8, 9, 10, 11, 12)))));
+        assertThat(take(0, source), iterates());
+        assertThat(take(1, source), iterates(1));
+        assertThat(take(6, source), iterates(1, 2, 3, 4, 6, 10));
+        assertThat(take(100, source), iterates(1, 2, 3, 4, 6, 10, 11, 12));
+    }
+
+    // drop
+    // concat
+    // concat/drop
+    // drop/take
+    // concat/take
+    // concat/drop/take
+
 }
